30578d65a2640d332aee962bb0498ac24d0714b1
[ad11tya1109] <[vinzxy71@gmail.com]>
2026-05-13 06:37:35 +0000

    fix: use service name for DB_HOST

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ cp .env.example .env || true
 php artisan key:generate
 
 # Konfigurasi database di .env
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST=172.17.0.2/g' .env
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST=mysql/g' .env
 sed -i 's/DB_PASSWORD=/DB_PASSWORD=password/g' .env
 
 # Jalankan migrasi jika diperlukan
